8c09aabbe4f9bbbb64dfe2a23c8355bd9f516c94
seungjae93 <lsj72023892@gmail.com>
2023-04-09 20:57:18 +0900

    :sparkles: [feat] 라우터 헤더 윤곽잡기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import ProductDetail from "./pages/ProductDetail";
+import NewProduct from "./pages/NewProduct";
+import MyCart from "./pages/MyCart";
+import NotFound from "./pages/NotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "/", element: <Home /> },
+      { path: "/products", element: <AllProducts /> },
+      { path: "/products/new", element: <NewProduct /> },
+      { path: "/products/:id", element: <ProductDetail /> },
+      { path: "/carts", element: <MyCart /> },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
